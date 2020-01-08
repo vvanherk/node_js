9176e9e5b85b5f8ad4dc7604093f9fc30c6412db
vvanherk <vanherk@mac.com>
2020-01-08 07:27:30 -0500

    undefined updated

---
+++ node-basics/undefined.js
@@ -0,0 +1,41 @@
+// var name = undefined;
+//
+// console.log(name);
+//
+//
+// function doesNothing(age) {
+//     console.log(age);
+// }
+//
+// //console.log(doesNothing());
+//
+// doesNothing();
+
+var name = undefined;
+var x;
+
+if (name === undefined){
+    console.log('name is undefined');
+}
+
+if (typeof x === 'undefined') {
+    console.log('x is undefined');
+}
+
+
+greetUser('Bob');
+
+greetUser();
+
+greetUser(1);
+
+
+
+function greetUser(name) {
+
+    if (typeof name === 'undefined') {
+        console.log('Hello World');
+    } else {
+        console.log('Hello ' + name);
+    }
+}
